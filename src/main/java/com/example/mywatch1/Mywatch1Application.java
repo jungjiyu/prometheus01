0e4ff4f6ffe66ed9c69609816b907a01291c87c0
jungjiyu <libraryofjiyu@gmail.com>
2024-08-20 17:11:07 +0900

    init

---
+++ src/main/java/com/example/mywatch1/Mywatch1Application.java
@@ -0,0 +1,13 @@
+package com.example.mywatch1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mywatch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mywatch1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mywatch1/Mywatch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mywatch1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mywatch1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
