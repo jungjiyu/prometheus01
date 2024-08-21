9b1aef8c88ef4e19346c2682e23c13480728f2e3
jungjiyu <142137932+jungjiyu@users.noreply.github.com>
2024-08-21 12:26:20 +0900

    add : actuator 추가

--- src/main/java/com/example/mywatch1/repository/ApiLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mywatch1.repository;
-
-import com.example.mywatch1.entity.ApiLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApiLogRepository extends JpaRepository<ApiLog, Long> {
-}
