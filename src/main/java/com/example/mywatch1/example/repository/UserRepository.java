84febae9c15ae92f9c215fe5bf7ccb2aec42e5c1
jungjiyu <libraryofjiyu@gmail.com>
2024-08-21 18:37:55 +0900

    add : 모니터링 당할 간단한 구조 추가

---
+++ src/main/java/com/example/mywatch1/example/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.mywatch1.example.repository;
+
+
+import com.example.mywatch1.example.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
